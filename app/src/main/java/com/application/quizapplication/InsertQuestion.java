32fc0c98050ccc5b50a61eeabed9bc7d477dd4b7
LazyJackyboy <skrok.jacek21@gmail.com>
2023-02-07 23:39:35 +0100

    setting connection with Firebase

---
+++ app/src/main/java/com/application/quizapplication/InsertQuestion.java
@@ -0,0 +1,14 @@
+package com.application.quizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_question);
+    }
+}
