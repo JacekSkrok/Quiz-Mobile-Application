c9355b1776decba39d843b4a2dc7b9212902b561
LazyJackyboy <skrok.jacek21@gmail.com>
2023-02-11 00:49:41 +0100

    add logic for adding question to db

--- app/src/main/java/com/application/quizapplication/InsertQuestion.java
+++ app/src/main/java/com/application/quizapplication/InsertQuestion.java
@@ -1,17 +1,73 @@
 package com.application.quizapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InsertQuestion extends AppCompatActivity {
+    private FirebaseDatabase aFirebaseDatabase;
+    private DatabaseReference aDatabaseReference;
+
+    EditText txtQuestiontext;
+    EditText txtAnswerA, txtAnswerB, txtAnswerC, txtAnswerD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_question);
+
+        aFirebaseDatabase = FirebaseDatabase.getInstance();
+        aDatabaseReference = aFirebaseDatabase.getReference().child("quizquestions");
+        txtQuestiontext = findViewById(R.id.questionName);
+        txtAnswerA = findViewById(R.id.answerA);
+        txtAnswerB = findViewById(R.id.answerB);
+        txtAnswerC = findViewById(R.id.answerC);
+        txtAnswerD = findViewById(R.id.answerD);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_menu:
+                addQuestion();
+                Toast.makeText(this, "Question added", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void clean() {
+        txtQuestiontext.setText("");
+        txtAnswerA.setText("");
+        txtAnswerB.setText("");
+        txtAnswerC.setText("");
+        txtAnswerD.setText("");
+        txtQuestiontext.requestFocus();
+    }
+
+    private void addQuestion() {
+        String questiontxt = txtQuestiontext.getText().toString();
+        String answerA = txtAnswerA.getText().toString();
+        String answerB = txtAnswerB.getText().toString();
+        String answerC = txtAnswerC.getText().toString();
+        String answerD = txtAnswerD.getText().toString();
+
+        QuizQuestion question = new QuizQuestion(questiontxt, answerA, answerB, answerC, answerD, "");
+
+        aDatabaseReference.push().setValue(question);
+
     }
 
     @Override
---
+++ app/src/main/java/com/application/quizapplication/QuizQuestion.java
@@ -0,0 +1,77 @@
+package com.application.quizapplication;
+
+public class QuizQuestion {
+    private String id;
+    private String questionText;
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String answerD;
+    private String imageUrl;
+
+    public QuizQuestion(String questionText, String answerA, String answerB, String answerC, String answerD, String imageUrl) {
+        this.setId(id);
+        this.setQuestionText(questionText);
+        this.setAnswerA(answerA);
+        this.setAnswerB(answerB);
+        this.setAnswerC(answerC);
+        this.setAnswerD(answerD);
+        this.setImageUrl(imageUrl);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
