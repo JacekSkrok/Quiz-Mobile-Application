9776e976f4fa8514bbe6d97a60a1c5266fa016e3
LazyJackyboy <skrok.jacek21@gmail.com>
2023-02-10 23:12:26 +0100

    add adding menu in toolbar

--- app/src/main/java/com/application/quizapplication/InsertQuestion.java
+++ app/src/main/java/com/application/quizapplication/InsertQuestion.java
@@ -3,6 +3,8 @@ package com.application.quizapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertQuestion extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class InsertQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_question);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
     }
 }
